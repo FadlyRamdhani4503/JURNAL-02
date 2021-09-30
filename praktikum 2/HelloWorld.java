26423eebecc4f00a6ccd1d3514019d4670de837a
FadlyRamdhani4503 <fadlicaos@gmail.com>
2021-09-30 23:06:37 +0700

    HelloWorld.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama 	: Fadly Ramdhani");
+		System.out.println("Nim 	: 6706213127");
+		System.out.println("Kelas 	: 4503");
+		}
+}
